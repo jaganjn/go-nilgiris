7b3f9262401092b265e9d915eae4c538a141cda1
Nagaraj <105140861+jaganjn@users.noreply.github.com>
2026-07-25 20:02:45 +0530

    Add Next.js configuration for image handling

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  poweredByHeader: false,
+  reactStrictMode: true,
+
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
